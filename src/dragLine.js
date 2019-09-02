f65b60021afbf8e6c7f77ed15e524bc32373cb0a
William_Gu <gujungujun@qq.com>
2019-09-02 12:25:00 +0800

    添加兼容性

--- src/dragLine.js
+++ src/dragLine.js
@@ -1,6 +1,6 @@
 class Line{
   constructor(line){
-    this.line = line
+    this.line = line;
     this.x = 0; //pageX
     this.y = 0; //pageY
     this.left = 0; //pageY
@@ -9,7 +9,7 @@ class Line{
   }
   getDom(){return this.line}
   getStyle(){
-    var style = window.getComputedStyle(this.line,null)
+    var style = window.getComputedStyle(this.line, null)
     return {
       left: parseInt(style.getPropertyValue("left")),
       right: parseInt(style.getPropertyValue("right"))
@@ -19,7 +19,36 @@ class Line{
     this.line.setAttribute('style',data)
   }
 }
+function initialDom(obj){
+  if(!obj.el){
+    obj.el = "separator-dom"
+  }
+  var separatorDom = document.getElementById(obj.el);
+  if(!separatorDom){
+    console.error("U Should set el: 'separator-dom'");
+    return false;
+  };
+
+  var vdragWrap = createElement("div", {id: "vdragWrap", class: "vdragWrap"})
+  
+  var dragLine = createElement("div", {
+    id: "dragLine",
+    class: "dragLine",
+  })
+
+  var vdragLine = createElement("div", {id: "vdragLine", class: "vdragLine"})
+  var vdragLineDashed = createElement("div", {id: "vdragLine-dashed", class: "vdragLine-dashed"})
+  createAppend(vdragLine, dragLine)
+  createAppend(vdragLineDashed, dragLine)
+  
+  createAppend(vdragWrap, separatorDom)
+  createAppend(dragLine, separatorDom)
+  return separatorDom;
+}
+
 function dragLine(obj) {
+  initialDom(obj); // 设立separator Dom元素
+
   var vdragWrap = document.querySelector('#vdragWrap');
   var Dom = document.querySelector('#dragLine'); //实际使用的Dom分割线
   var DomLine = new Line(Dom);
@@ -50,7 +79,7 @@ function dragLine(obj) {
       var excursionX =  e.clientX - vDomLine.x;
       console.log(vDomLine.left , excursionX);
       
-      if((vDomLine.left + excursionX)>=310 && (vDomLine.left + excursionX)<=570){
+      if((vDomLine.left + excursionX) >= obj.min && (vDomLine.left + excursionX) <= obj.max){
         vDomLine.excursionX = excursionX;
         vDomDashedLine.setStyle(lineStyle + 'transform:translateX(' + excursionX + 'px)')
       }
@@ -100,3 +129,35 @@ function removeClass(ele, cls) {
     ele.className = newClass.replace(/^\s+|\s+$/g, '');
   }
 }
+function createElement(elementName, obj) {
+  var element = document.createElement(elementName);
+
+  for (let key in obj) {
+    if (key === "class") {
+      element.className = obj[key];
+    } else if (key === "style") {
+      if (element.setAttribute) {
+        element.setAttribute("style", obj[key]);
+      } else {
+        element.style.cssText = obj[key];
+      }
+    } else {
+      element[key] = obj[key];
+    }
+  }
+  return element;
+}
+function createAppend(node, parent) {
+  let nodeDom;
+
+  if (node.nodeType !== 1 && typeof node === "string") {
+    nodeDom = document.createElement("div");
+    nodeDom.className = node.replace(/^\./, "");
+  } else {
+    nodeDom = node;
+  }
+  parent.appendChild(nodeDom);
+  return nodeDom;
+}
+
+// export default dragLine;
